c6ef0e970d51ce94f1ab14cc3c6908fc74389577
jiy0-0nv <jiyoon@yonsei.ac.kr>
2025-01-24 14:57:12 +0900

    [feat] #10 - Add init

---
+++ Project/APIs/__init__.py
@@ -0,0 +1,7 @@
+from .ClovaOCR import OCR
+from .ClovaPapago import Translate
+from .ClovaVoice import naver_tts
+from .HCXtunning import hcx_tunning
+from .HCXwords import make_words
+from .ImgAlt import img_alt
+from .UserInput import userInput

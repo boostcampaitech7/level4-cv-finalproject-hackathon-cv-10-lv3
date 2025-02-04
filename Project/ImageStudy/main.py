c4e31ac22e32762645a20f7c7047824fb93a2c7c
0seoYun <yys020819@gmail.com>
2025-02-04 15:51:43 +0900

    [feat] #18 - setting for async.

--- Project/ImageStudy/main.py
+++ Project/ImageStudy/main.py
@@ -1,5 +1,6 @@
 from .Study.chat import chat
 from .Study.diary import diary
+import asyncio
 
 
 # 1.image 촬영/불러오기
@@ -20,3 +21,22 @@ while True:
         break
     else:
         print("잘못된 입력입니다.")
+
+# async def main():
+#     while True:
+#         print("학습 방법을 선택하세요.")
+#         print("Chat-C, Diary-D, 종료-Q")
+#         StudyType = input().strip().lower()
+
+#         if StudyType == "c":
+#             await chat(ImagePath)
+#         elif StudyType == 'd':
+#             await diary()
+#         elif StudyType == 'q':
+#             print("학습을 종료합니다.")
+#             break
+#         else:
+#             print("잘못된 입력입니다.")
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
